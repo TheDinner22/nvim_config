f992a3faafd3e712cb0fbc2f07276bd9fd1d6852
joe goodman <jgood1133@gmail.com>
2023-02-10 20:11:15 -0500

    lsp setup

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,38 @@
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+local cmp = require("cmp")
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	["<C-Space>"] = cmp.mapping.complete()
+}) 
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>ls", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
+
