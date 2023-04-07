c5669f6cdeb74c67a84f0d38649f3456e96e4794
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-04-07 18:36:20 -0400

    auto close brackets

--- lua/thedinner/remap.lua
+++ lua/thedinner/remap.lua
@@ -40,3 +40,9 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+-- auto close {}, [], and (), and <>?
+vim.keymap.set("i", "{", "{}<esc>i")
+vim.keymap.set("i", "[", "[]<esc>i")
+vim.keymap.set("i", "(", "()<esc>i")
+vim.keymap.set("i", "<", "<><esc>i")
