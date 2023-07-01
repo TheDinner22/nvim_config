8bc3d0a159a79f5c2b524e41fce720f4f9eb96c8
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-07-01 10:09:06 -0400

    commented auto closing brakets and cmake command

--- lua/thedinner/remap.lua
+++ lua/thedinner/remap.lua
@@ -36,13 +36,15 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- run makefile (will brake if not in a cpp project)
+vim.keymap.set("n", "<leader>m", "<cmd>!make<CR>")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
 -- auto close {}, [], and (), and <>?
-vim.keymap.set("i", "{", "{}<esc>i")
-vim.keymap.set("i", "[", "[]<esc>i")
-vim.keymap.set("i", "(", "()<esc>i")
-vim.keymap.set("i", "<", "<><esc>i")
+-- vim.keymap.set("i", "{", "{}<esc>i")
+-- vim.keymap.set("i", "[", "[]<esc>i")
+-- vim.keymap.set("i", "(", "()<esc>i")
+-- vim.keymap.set("i", "<", "<><esc>i")
