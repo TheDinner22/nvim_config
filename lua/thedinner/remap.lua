4041270f0344849c7af4c331bb8e845efefe502d
Joseph Handsome <nottellingu@gmail.com>
2024-11-01 12:41:59 -0400

    added ccache

--- lua/thedinner/remap.lua
+++ lua/thedinner/remap.lua
@@ -40,7 +40,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 -- run makefile (will brake if not in a cpp project)
-vim.keymap.set("n", "<leader>m", "<cmd>!make<CR>")
+vim.keymap.set("n", "<leader>m", "<cmd>!make CXX='ccache g++'<CR>")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
