1cbaa50fa3cf24ecaa38a5ce4ec2ff2ce39740b9
Joseph Handsome <nottellingu@gmail.com>
2025-04-18 16:43:13 -0400

    added bat to view git diff super cool but not really usefull

--- lua/thedinner/remap.lua
+++ lua/thedinner/remap.lua
@@ -13,6 +13,38 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- fighting one eyed kirby
 vim.keymap.set("v", "<leader>fe", ":s/\\(.*\\)/thing")
 
+vim.keymap.set("n", "<leader>d", function()
+  local filepath = vim.api.nvim_buf_get_name(0)
+  if filepath == "" then
+    print("No file found.")
+    return
+  end
+
+  -- Create a new scratch buffer
+  local buf = vim.api.nvim_create_buf(false, true)
+
+  -- Floating window dimensions
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.6)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+
+  -- Open floating window
+  vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = "minimal",
+    border = "rounded",
+  })
+
+  -- Run bat on the current file *inside the terminal buffer*
+  vim.fn.termopen({ "bat", "--paging=always", filepath })
+  vim.cmd("startinsert")
+end)
+
 -- greatest remap ever
 vim.keymap.set("x", "<leader>P", [["_dP]])
 
@@ -27,7 +59,7 @@ vim.keymap.set("x", "<leader>P", [["+P]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
@@ -41,6 +73,9 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 -- run makefile (will brake if not in a cpp project)
 vim.keymap.set("n", "<leader>m", "<cmd>!make CXX='ccache g++'<CR>")
+
+-- run ./run.sh
+vim.keymap.set("n", "<leader>n", "<cmd>!./run.sh<CR>")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
