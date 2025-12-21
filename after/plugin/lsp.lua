72ac0e4ea82f32a38c1bdc6bbfce37690c326e48
Joseph Handsome <dontemailme@nah.com>
2025-12-21 13:25:07 -0500

    maybe now?

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -23,6 +23,25 @@ cmp.setup({
         ['<C-b>'] = cmp_action.luasnip_jump_backward(),
         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
+	["<Tab>"] = cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_next_item()
+		elseif luasnip.expand_or_jumpable() then
+			luasnip.expand_or_jump()
+		else
+			fallback()
+		end
+	end, {"i", "s"}),
+
+	["<S-Tab>"] = cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_prev_item()
+		elseif luasnip.jumpable(-1) then
+			luasnip.jump(-1)
+		else
+			fallback()
+		end
+	end, {"i", "s"}),
     }),
     snippet = {
         expand = function(args)
