8ba93a2fbc9d1b7668a20d8050e47f1f35fe0aef
Joseph Handsome <dontemailme@nah.com>
2026-02-08 20:13:27 -0500

    chat tries to add clangd :)

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,11 +4,9 @@ local lsp = require('lsp-zero').preset({
   manage_nvim_cmp = false,
   suggest_lsp_servers = false,
 })
-
 local cmp = require("cmp")
 local luasnip = require('luasnip')
 local cmp_action = require('lsp-zero').cmp_action()
-
 cmp.setup({
     window = {
         completion = cmp.config.window.bordered(),
@@ -24,25 +22,24 @@ cmp.setup({
         ['<C-b>'] = cmp_action.luasnip_jump_backward(),
         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
-	["<Tab>"] = cmp.mapping(function(fallback)
-		if cmp.visible() then
-			cmp.select_next_item()
-		elseif luasnip.expand_or_jumpable() then
-			luasnip.expand_or_jump()
-		else
-			fallback()
-		end
-	end, {"i", "s"}),
-
-	["<S-Tab>"] = cmp.mapping(function(fallback)
-		if cmp.visible() then
-			cmp.select_prev_item()
-		elseif luasnip.jumpable(-1) then
-			luasnip.jump(-1)
-		else
-			fallback()
-		end
-	end, {"i", "s"}),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+        else
+            fallback()
+        end
+    end, {"i", "s"}),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+        else
+            fallback()
+        end
+    end, {"i", "s"}),
     }),
     snippet = {
         expand = function(args)
@@ -50,16 +47,14 @@ cmp.setup({
         end,
     },
     sources = {
-        { name = 'nvim_lsp' },  -- ← THIS is the big one
+        { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'buffer' },
         { name = 'path' },
     },
 })
-
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
-
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>ls", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -70,66 +65,38 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
--- (Optional) Configure lua language server for neovim
 local lua_opts = lsp.nvim_lua_ls()
 require('lspconfig').lua_ls.setup(lua_opts)
 
--- lsp.configure("clangd", {
-  -- cmd = { 
-    -- "clangd", 
-    -- "--background-index", 
-    -- "--clang-tidy", 
-    -- "--compile-commands-dir=build", -- Add this line
-    -- "--query-driver=/usr/bin/g++,/usr/bin/gcc,/usr/bin/c++"
-  -- },
-  -- filetypes = { "c", "cpp", "objc", "objcpp", "hpp" },
-  -- root_dir = require("lspconfig.util").root_pattern(
-    -- "compile_commands.json", -- Add this to look for compile_commands.json
-    -- ".git"
-  -- ),
-  -- capabilities = require("cmp_nvim_lsp").default_capabilities(),
-  -- init_options = {
-    -- clangdFileStatus = true,
-    -- usePlaceholders = true,
-    -- completeUnimported = true,
-  -- },
--- })
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- after your current lsp setup/config for clangd, but before final lsp.setup()
--- require('lspconfig').ocamllsp.setup({
-  -- cmd = { "ocamllsp" },
-  -- filetypes = {
-    -- "ocaml",
-    -- "ocaml.interface",
-    -- "ocaml.menhir",
-    -- "ocaml.ocamllex",
-    -- "reason",
-    -- "dune",
-  -- },
-  -- root_dir = require("lspconfig.util").root_pattern(
-    -- "dune-project", "dune-workspace", "*.opam", "esy.json", "package.json", ".git"
-  -- ),
-  -- settings = {},  -- you can optionally add ocamllsp-specific settings here
-  -- on_attach = function(client, bufnr)
-    -- -- reuse your existing keymaps (or call your shared on_attach)
-    -- local opts = { buffer = bufnr, remap = false }
-    -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    -- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    -- vim.keymap.set("n", "<leader>ls", vim.lsp.buf.workspace_symbol, opts)
-    -- vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts)
-    -- vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
-    -- vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-    -- vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
-    -- vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-  -- end,
--- })
+lsp.configure("clangd", {
+  cmd = { 
+    "clangd", 
+    "--background-index", 
+    "--clang-tidy", 
+    "--compile-commands-dir=build",
+    "--query-driver=/usr/bin/g++,/usr/bin/gcc,/usr/bin/c++"
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp" },
+  root_dir = require("lspconfig.util").root_pattern(
+    "compile_commands.json",
+    ".git"
+  ),
+  capabilities = capabilities,
+  init_options = {
+    clangdFileStatus = true,
+    usePlaceholders = true,
+    completeUnimported = true,
+  },
+})
 
 lsp.configure("ocamllsp", {
   cmd = { "ocamllsp" },
 })
 
 lsp.configure("rust_analyzer", {
-  cmd = { "rust-analyzer" }, -- MUST be the Nix one in PATH
+  cmd = { "rust-analyzer" },
   filetypes = { "rust" },
   root_dir = require("lspconfig.util").root_pattern(
     "Cargo.toml",
@@ -146,8 +113,6 @@ lsp.configure("rust_analyzer", {
   },
 })
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
 lsp.configure("svls", {
   cmd = { "svls" },
   filetypes = { "verilog", "systemverilog" },
@@ -159,8 +124,6 @@ lsp.configure("svls", {
 })
 
 lsp.setup()
-
 vim.diagnostic.config({
     virtual_text = true
 })
-
